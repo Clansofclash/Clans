de572bd3a16d4f98abe98080ed58fcc3cd96a87f
arnabghatak22 <arnabghatak22@gmail.com>
2018-03-24 11:20:21 +0530

    Added Controller Class

--- src/main/java/AgeofClans/App.java
+++ src/main/java/AgeofClans/App.java
@@ -1,31 +1,19 @@
 package AgeofClans;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import controller.Controller;
 
 /**
  * Hello world!
  *
  */
-@RestController
-@EnableAutoConfiguration
 public class App 
 {
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
-    @RequestMapping("/abc")
-    String homertr() {
-        return "Hello World!hhhhhhhhhhh";
-    }
 
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(Controller.class, args);
         //System.out.println( "Hello World!" );
     }
 }
---
+++ src/test/java/controller/ControllerTest.java
@@ -0,0 +1,6 @@
+package controller;
+
+public class ControllerTest {
+
+	// Too lazy to write test cases now
+}
