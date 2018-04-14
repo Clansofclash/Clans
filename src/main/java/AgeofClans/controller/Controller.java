047f6e13eafa687473a916ec5fd1cddf3ab88188
Clans <clashthedevil@gmail.com>
2018-04-14 19:49:32 +0530

    adding dao test code and refactoring

--- src/main/java/AgeofClans/controller/Controller.java
+++ src/main/java/AgeofClans/controller/Controller.java
@@ -1,15 +1,26 @@
-package controller;
+package AgeofClans.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import AgeofClans.dao.MainDAO;
 
 @RestController
 @EnableAutoConfiguration
 public class Controller {
 
+    //@Autowired
+    MainDAO mainDAO;
+
     @RequestMapping("/abc")
     String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/db")
+    String test()
+    {
+        return mainDAO.test();
     }
 }
---
+++ src/main/java/AgeofClans/dao/MainDAO.java
@@ -0,0 +1,28 @@
+package AgeofClans.dao;
+
+import AgeofClans.db.DbConnect;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MainDAO {
+
+    @Autowired
+    DbConnect dbConnect;
+
+    public String test()
+    {
+        try {
+            Connection connection = dbConnect.getConnection();
+            return "Succes";
+        } catch (URISyntaxException e) {
+            //e.printStackTrace();
+            return e.toString();
+        } catch (SQLException e) {
+            //e.printStackTrace();
+            return e.toString();
+        }
+    }
+}
