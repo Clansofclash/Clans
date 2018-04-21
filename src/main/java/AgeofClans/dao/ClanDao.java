0c8faa68c3cc4638d272fd06c344916c7f812231
Clans <clashthedevil@gmail.com>
2018-04-22 00:52:12 +0530

    some test code to create a table. Would  update more later

--- src/main/java/AgeofClans/dao/ClanDao.java
+++ src/main/java/AgeofClans/dao/ClanDao.java
@@ -1,4 +1,15 @@
 package AgeofClans.dao;
 
 public class ClanDao {
+
+    private static String tableCreatecmd = "CREATE TABLE POI(\n" +
+            "   ID INT PRIMARY KEY      NOT NULL,\n" +
+            "   NAME           CHAR(50) NOT NULL,\n" +
+            "   LEVEL          INT      NOT NULL\n" +
+            "   TC_ID          INT      NOT NULL\n" +
+            "   GOLD           INT      NOT NULL\n" +
+            "   GOLD_RATE      INT      NOT NULL\n" +
+            "   GOD_ID         INT      NOT NULL\n" +
+            "   HAPPINESS      FLOAT    NOT NULL\n" +
+            ");";
 }
