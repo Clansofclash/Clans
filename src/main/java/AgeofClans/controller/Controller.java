1a0d368c649f18320787f1d7906719517fb8b075
Clans <clashthedevil@gmail.com>
2018-04-21 19:32:47 +0530

    adding dao drop table code

--- src/main/java/AgeofClans/controller/Controller.java
+++ src/main/java/AgeofClans/controller/Controller.java
@@ -2,9 +2,13 @@ package AgeofClans.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import AgeofClans.dao.MainDAO;
+
+import java.net.URISyntaxException;
+import java.sql.SQLException;
 
 @RestController
 @EnableAutoConfiguration
@@ -21,5 +25,11 @@ public class Controller {
     {
         MainDAO mainDAO = new MainDAO();
         return mainDAO.test();
+    }
+
+    @RequestMapping("/db/drop/table/{tableName}")
+    String dropTable(@PathVariable String tableName) throws URISyntaxException, SQLException {
+        MainDAO mainDAO = new MainDAO();
+        return mainDAO.dropTable(tableName);
     }
 }
--- src/main/java/AgeofClans/dao/MainDAO.java
+++ src/main/java/AgeofClans/dao/MainDAO.java
@@ -3,9 +3,11 @@ package AgeofClans.dao;
 import AgeofClans.db.DbConnect;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.xml.ws.Response;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainDAO {
 
@@ -32,5 +34,22 @@ public class MainDAO {
             return "SQL Fucked!! "+e.toString();
         }
         //return "no joy";
+    }
+
+    //TODO replace return types with responses
+    public String dropTable(String tableName) throws URISyntaxException, SQLException {
+
+        Connection connection = dbConnect.getConnection();
+        Statement statement = connection.createStatement();
+        try
+        {
+            String cmd = "DROP TABLE "+tableName+" IF EXISTS";
+            statement.executeUpdate(cmd);
+            return "Done";
+        }
+        catch (Exception e)
+        {
+            return "Failure!! "+e.toString();
+        }
     }
 }
